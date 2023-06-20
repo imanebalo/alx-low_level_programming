876ac03fba781c8da257ff89316c1904700c5e7c
balouitiimanee@gmail.com <imanebalo>
2023-06-20 11:27:09 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Function that adds two integers and returns the result
+ * @a: the number to be add
+ * @b: the number to be add
+ * Return: c Success
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
