e2d103c01ce26effa596d40e56acac29b366f92e
balouitiimanee@gmail.com <imanebalo>
2023-06-23 14:09:17 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 /**
  * print_numbers - Function that prints the numbers from 0 to 9
  * followed by a new line
- * Return: 0 Success
  */
 void print_numbers(void)
 {
@@ -12,5 +11,4 @@ for (i = '0'; i <= '9'; i++)
 _putchar(i);
 }
 _putchar('/n');
-return (0);
 }
