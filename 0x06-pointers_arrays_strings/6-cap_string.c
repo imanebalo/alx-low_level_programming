edc636e3ade698b774d357022c92300438899774
balouitiimanee@gmail.com <imanebalo>
2023-07-02 13:40:17 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,22 +6,24 @@
  */
 char *cap_string(char *src)
 {
-int n = 0;
-int i;
-while (src[n] != '\0')
+int i = 0, j;
+char a[] = " \t\n,;.!?\"(){}";
+while (*(src + i))
 {
-n++;
+if (*(src + i) >= 'a' && *(src + i) <= 'z')
+{
+if (i == 0)
+*(src + i) -= 'a' - 'A';
+else
+{
+for (j = 0; j <= 12; j++)
+{
+if (a[j] == *(src + i - 1))
+*(src + i) -= 'a' - 'A';
 }
-for (i = 0; i < n; i++)
-{
-if (i == 0 || src[i - 1] == ' ' || src[i - 1] == '\n' ||
-src[i - 1] == '\t' || src[i - 1] == ',' || src[i - 1] == ';' || 
-src[i - 1] == '.' || src[i - 1] == '!' || src[i - 1] == '?' || 
-src[i - 1] == '"' || src[i - 1] == '(' || src[i - 1] == ')' || 
-src[i - 1] == '{' || src[i - 1] == '}')
-{
-src[i] = src[i] - 32;
 }
+}
+i++;
 }
 return (src);
 }
